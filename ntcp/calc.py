7f9c2a66e48425c3e8281736e7f0370d1c1494ca
seohan <rlaqorms1229@naver.com>
2023-10-26 11:50:52 +0900

    make class

--- ntcp/calc.py
+++ ntcp/calc.py
@@ -2,34 +2,42 @@ import numpy as np
 import pandas as pd
 import math
 
-def diffDVH2arr(fin):
-    df = pd.read_excel(fin,header=0,index_col=None)
-    dose = df.iloc[:,0]
-    vol = df.iloc[:,1]
-    arr = np.vstack((dose,vol))
-    return arr
+class calc:
+    __slots__ = ["fin","geud","ntcp","dvh_arr","a","d50","m"]
+    def __init__(self):
+        self.fin = {}
+        self.geud = {}
+        self.ntcp = {}
+        self.dvh_arr = {}
+        self.a = {}
+        self.d50 = {}
+        self.m = {}
 
-def geud(dvh_arr):
-    a = 23.274
-    dose = dvh_arr[0,:]
-    vol = dvh_arr[1,:]
-    sum = 0
-    for i,j in zip(dose,vol):
-        sum += ((i)**a) * j
-    sol = (sum)**(1/a)
-    return sol
+    def diffDVH2arr(self, fin):
+        self.fin =fin
+        df = pd.read_excel(self.fin,header=0,index_col=None)
+        dose = df.iloc[:,0]
+        vol = df.iloc[:,1]
+        self.dvh_arr = np.vstack((dose,vol))
+        return self.dvh_arr
 
-def ntcp(dvh_arr):
-    d50 = 6.235
-    m = 0.498
-    dose = geud(dvh_arr)
-    ntcp = 1 / 2 * (1 + math.erf((dose - d50) / (m * d50 * (2 ** (1 / 2)))))
-    return ntcp
+    def get_geud(self, dvh_arr, a = 23.274):
+        self.a = a
+        dose = dvh_arr[0,:]
+        vol = dvh_arr[1,:]
+        sum = 0
+        for i,j in zip(dose,vol):
+            sum += ((i)**self.a) * j
+        self.geud = (sum)**(1/self.a)
+        return self.geud
+
+    def get_ntcp(self, dose, d50=6.235, m=0.498):
+        self.d50 = d50
+        self.m = m
+        self.ntcp = 1 / 2 * (1 + math.erf((dose - self.d50) / (self.m * self.d50 * (2 ** (1 / 2)))))
+        return self.ntcp
 
 
-
-
-fin = "cum.xlsx"
 
 
 
--- ntcp/example.py
+++ ntcp/example.py
@@ -1,3 +1,7 @@
-import calc
+from calc import calc
 
-
+c = calc()
+arr = c.diffDVH2arr("7.xlsx")
+geud = c.get_geud(arr) # default a = 23.274
+ntcp = c.get_ntcp(geud) # default d50 = 6.235, m = 0.498
+print(ntcp)
