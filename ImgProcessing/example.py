29e7eeb097ea78fb5e33b3e9a400297033d57ad0
seohan <rlaqorms1229@naver.com>
2023-10-26 17:02:10 +0900

    make class

--- ImgProcessing/example.py
+++ ImgProcessing/example.py
@@ -1,19 +1,22 @@
+'''Instruction on image processing class
+    Using nifti format is recommended for convenience of handling files'''
 import matplotlib.pyplot as plt
 from ImgProcessing import ImgProcessing
 
 fin = "/Volumes/data-1/ksh/pelvic_RIL/raw_data/1/ct/"
+mask_fin = "/Volumes/data-1/ksh/pelvic_RIL/raw_data/1/contour/2.16.840.1.114362.1.12148098.22261327066.644512811.711.5147.dcm"
 
 i = ImgProcessing()
-i.dcm2nii(fin)
+i.dcm2nii(fin)                      #Default: write_img = False, output_fin = 'converted_ct.nii'
 ct_arr = i.ct_arr
-ct = i.ct_nii
+ct_nii = i.ct_nii
 
-mask_fin = "/Volumes/data-1/ksh/pelvic_RIL/raw_data/1/contour/2.16.840.1.114362.1.12148098.22261327066.644512811.711.5147.dcm"
-i.mask2arr(ct,mask_fin,'liver')
+i.mask2arr(ct_nii,mask_fin,'liver')     #Default: write_img = False, output_fin = 'extracted_mask.nii'
 mask_arr = i.mask_arr
+mask_nii = i.mask_nii
 
+'''result plot'''
 fig, ax = plt.subplots(1,2)
-
 ax[0].matshow(ct_arr[:,250,:])
 ax[1].matshow(mask_arr[:,250,:])
 plt.show()
--- ntcp/example.py
+++ ntcp/example.py
@@ -1,16 +1,18 @@
+''' Instruction on how to get ntcp
+    please check dvh_example format '''
 from calc import calc
-'''
+
 c = calc()
-arr = c.diffDVH2arr("7.xlsx") # arr = [[dose],[volume]]
-c.get_geud(c.dvh_arr) # default a = 23.274 /when a is set to 1, it is equal to mean dose.
-c.get_ntcp(c.geud) # default d50 = 6.235, m = 0.498
+arr = c.diffDVH2arr("dvh_example.xlsx")     # arr = [[dose],[volume]]
+c.get_geud(c.dvh_arr, a=23.274)                       # default a = 23.274 /when a is set to 1, it is equal to mean dose.
+c.get_ntcp(c.geud, d50=6.235)                          # default d50 = 6.235, m = 0.498
 print(c.ntcp)
-'''
-'''
+
+
+'''ntcp plot'''
 from calc import calc
 import matplotlib.pyplot as plt
 
-# ntcp whole curve plot
 c2 = calc()
 x = range(0,20)
 y = [c2.get_ntcp(i) for i in x]
@@ -18,4 +20,3 @@ plt.plot(x,y)
 plt.xlabel("Dose [Gy]")
 plt.ylabel("Normal tissue complication probability")
 plt.show()
-'''
